e0982b07408413320b1bfc86ed9dbeb84ef3d16b
Caleb Tonn <calebtonn@calebs-mac-studio.local>
2023-08-30 12:28:00 -0700

    wip

--- data/create_example_csv.py
+++ data/create_example_csv.py
@@ -11,7 +11,7 @@ import MySQLdb
 db = MySQLdb.connect(
     host="localhost",  # your host, usually localhost
     user="john",  # your username
-    passwd="megajonhy",  # your password
+    passwd="megajonhy!",  # your password
     db="jonhydb",
 )  # name of the data base
 
